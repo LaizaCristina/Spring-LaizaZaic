ffc95a15ce30c6817cd3468d2e7a1a85176753fd
Laiza Zaic <111503805+LaizaCristina@users.noreply.github.com>
2024-08-13 09:41:31 -0300

    Add files via upload

---
+++ LaizaZaic/src/test/java/_Desk/LaizaZaic/LaizaZaicApplicationTests.java
@@ -0,0 +1,13 @@
+package _Desk.LaizaZaic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaizaZaicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
